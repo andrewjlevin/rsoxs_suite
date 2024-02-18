a7442a5e5519b0324ee9f0b67d2d98e44497e960
Andrew Levin <andrew.levin@colorado.edu>
2024-02-17 23:12:32 -0700

    Delete learning_resources directory
    this should not affect the local directories as this is in the gitignore...

--- learning_resources/morph_gen_camille/importing_python.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.insert(1, '/home/ceb10/Jan2023/')
-from dean_reduce06 import *
-
-lfoo, lbar = cyrsoxs_datacubes('/home/ceb10/Jan2023//', 1.95, 2048, 2048)
-
-print(lbar)
-lbar.to_netcdf('/home/ceb10/Jan2023//tempS0p5.nc')
